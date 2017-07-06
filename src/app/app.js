a9c6e76ac351661c1094a9de40ca138b1d1d8634
Gian Dolotina <gpdolotina@gmail.com>
2017-07-06 11:31:52 -1100

    Added line graphs using CanvasJS

--- src/app/app.js
+++ src/app/app.js
@@ -3,19 +3,6 @@ import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
 	render() {
-		const clientStyle = {
-			height: "500px"
-		};
-
-		const otherStyle = {
-			backgroundColor: "#FF0000",
-			color: "#d2d0d0",
-			marginTop: "-30px"
-		};
-
-		const otherContainerStyle = {
-			marginTop: "-20px"
-		};
 
 		var monthlyIncome = 12000.00;
 		var monthlyExpense = 4598.00;
@@ -28,33 +15,40 @@ class Layout extends React.Component {
 					<div class="container">
 						<div class="row">
 							<div class="col-md-4">
-								<h3><i class="fa fa-money"></i> Monthly Income: ₱ {monthlyIncome}</h3>			
+								<h3><i class="fa fa-money"></i> Monthly Income: ₱ {monthlyIncome}</h3>
+								<br/>
+								<br/>
+								<div class="container" id="chartIncome"></div>		
 							</div>
 							<div id="box-shadow" class="col-md-4">
-								<h3><i class="fa fa-line-chart"></i> Monthly Expense: ₱ {monthlyExpense}</h3>			
+								<h3><i class="fa fa-line-chart"></i> Monthly Expense: ₱ {monthlyExpense}</h3>
+								<br/>
+								<br/>
+								<div class="container" id="chartExpense"></div>		
 							</div>
 							<div id="box-shadow" class="col-md-4">
-								<h3><i class="fa fa-credit-card"></i> Monthly Savings: ₱ {monthlySavings}</h3>			
+								<h3><i class="fa fa-credit-card"></i> Monthly Savings: ₱ {monthlySavings}</h3>
+								<br/>
+								<br/>
+								<div class="container" id="chartSavings"></div>			
 							</div>
 						</div>
 						<br/>
 						<div class="row">
-							<div style={{textAlign:"left"}} class="col-md-8">
-								<h2>₱esOf Mind</h2>
-								<p>was founded in 2017, focusing on the management of income flow giving
-								 people the ability to track their own expenses.</p>
+							<div class="col-md-1">
+
 							</div>
-							<div class="col-md-4">
+							<div class="col-md-8">
+								
+							</div>
+							<div class="col-md-1">
 
 							</div>
 						</div>
 						<div class="row">
-							<div class="col-md-4">
-								
-							</div>
-							<div style={{textAlign:"right"}} class="col-md-8">
-								<h2>₱esOf Mind</h2>
-								<p>was founded in 2017, focusing on the management of income flow giving
+							<div style={{textAlign:"justify"}} class="col-md-8">
+								<h2>About Us</h2>
+								<p>₱esOf Mind was founded in 2017, focusing on the management of income flow giving
 								 people the ability to track their own expenses.</p>
 							</div>
 						</div>
@@ -62,13 +56,15 @@ class Layout extends React.Component {
 							<div class="col-md-4">
 								<h2>Contact Us</h2>
 								<br/>
-								<input type="email" class="form-control" name="contactEmail" placeholder="E-mail Address"></input>
-								<br/>
-								<input type="text" class="form-control" name="contactName" placeholder="Full Name"></input>
-								<br/>
-								<input type="text-area" class="form-control" name="contactMessage" placeholder="Message"></input>
-								<br/>
-								<button class="btn btn-primary">Submit</button>
+								<form method="post" action="">
+									<input type="email" class="form-control" name="contactEmail" placeholder="E-mail Address"></input>
+									<br/>
+									<input type="text" class="form-control" name="contactName" placeholder="Full Name"></input>
+									<br/>
+									<textarea class="form-control" rows="5" name="contactMessage" placeholder="Message"></textarea>
+									<br/>
+									<button class="btn btn-primary">Submit</button>
+								</form>
 							</div>
 						</div>
 					</div>
